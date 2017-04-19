fa642120f34c600a89f10f8a54d57825f733b0fe
Kevin Fronczak <kfronczak@gmail.com>
2017-04-19 15:52:52 -0400

    Added ability to search and replace in ADEXL datasheets

--- bulk_convert.pl
+++ bulk_convert.pl
@@ -8,6 +8,7 @@
 #       a given extension to an eps file
 #
 # CHANGELOG:
+#   v0.2.0: Added serach and replace for xml files (adexl) (2017-04-19 KF)
 #   v0.1.0: Added general ext to ext conversion (2017-01-11 KF)
 #   v0.0.3: Fixed man page (2017-01-04 KF)
 #   v0.0.2: Improved conversion command (2017-01-03 KF)
@@ -31,6 +32,7 @@ my $noTrim  = 0;
 my $noFlat  = 0;
 my $dpi     = 300;
 my $dest    = '';
+my $clean   = 0;
 
 GetOptions('verbose' => \$verbose,
            'force'   => \$force,
@@ -40,22 +42,35 @@ GetOptions('verbose' => \$verbose,
            'notrim'  => \$noTrim,
            'noflat'  => \$noFlat,
            'help|?'  => \$help,
+           'clean'   => \$clean,
            man       => \$man) or pod2usage(2);
 
 pod2usage(2) if $help;
 pod2usage(-exitval => 0, -verbose => 2) if $man;
 
-unless( $#ARGV == 2)
-{
-  die "\nERROR: Not enough arguments\nUsage: bulk_convert [opts] <source_ext> <convert_ext> <dir>\nUse '-man' option for more info\n";
-}
-
 my $startExt  = $ARGV[0];
 my $endExt    = $ARGV[1];
 my $dir       = $ARGV[2];
+my $findReplace = 0;
+
+# Note, this mode is undocumented.
+# Used only to convert ADEXL xml references to png
+if ($startExt =~ "xml")
+{
+  $dir = $endExt;
+  $findReplace = 1;
+  print $findReplace."\n";
+}
+else
+{
+  unless( $#ARGV == 2)
+  {
+    die "\nERROR: Not enough arguments\nUsage: bulk_convert [opts] <source_ext> <convert_ext> <dir>\nUse '-man' option for more info\n";
+  }
+}
 
 # Create bceps directory if needed
-if ( $dest eq '')
+if ( $dest eq '' && !$findReplace)
 {
   $dest = $ENV{"HOME"} . "/bceps";
   unless( -e $dest or mkdir $dest)
@@ -70,75 +85,89 @@ unless( $level < 4 && $level >= 0)
   die "ERROR: Unsupported eps level $level\n\n";
 }
 
-if ($verbose)
+if ($verbose && !$findReplace)
 {
   print "Looking for files with .$startExt in $dir and saving to $dest\n";
 }
 
 # Create a list of files in directory
 my @filesToConvert;
-find({wanted => \&find_file, no_chdir=>1}, $dir);
+if (!$findReplace)
+{
+  find({wanted => \&find_file, no_chdir=>1}, $dir);
 
-# Generate eps convert command
-my $epsCmd;
-if ($level == 0) { $epsCmd = "eps"; } 
-else { $epsCmd = "eps$level"; }
-
-
-my $cmdStart;
-my $cmdEnd;
-my $flat;
-my $trim;
-
-if ($noTrim) { $trim = "";         }
-else         { $trim = "-trim";    }
-if ($noFlat) { $flat = "";         }
-else         { $flat = "-flatten"; }
+  # Generate eps convert command
+  my $epsCmd;
+  if ($level == 0) { $epsCmd = "eps"; } 
+  else { $epsCmd = "eps$level"; }
 
 
-$cmdStart = "convert -density $dpi $trim";
-$cmdEnd   = "-quality 100 $flat";
-
-
-# Iterate over all files and convert to eps
-foreach my $file (@filesToConvert)
-{ 
-  my $newFile =  $file;
-  $newFile    =~ s/^$dir//;      # removes base directory from beginning of file name
-  $newFile    =~ s/$startExt$/$endExt/;   # Appends new file with .<convert_ext> instead of .<source_ext>
-  $newFile    =  $dest.$newFile;
-     
-  my $newDir = dirname($newFile);
+  my $cmdStart;
+  my $cmdEnd;
+  my $flat;
+  my $trim;
   
-  if (! -f $newFile || $force)
-  {
+  if ($noTrim) { $trim = "";         }
+  else         { $trim = "-trim";    }
+  if ($noFlat) { $flat = "";         }
+  else         { $flat = "-flatten"; }
   
-    unless(-e $newDir or make_path($newDir))
+  
+  $cmdStart = "convert -density $dpi $trim";
+  $cmdEnd   = "-quality 100 $flat";
+
+
+  # Iterate over all files and convert to eps
+  foreach my $file (@filesToConvert)
+  { 
+    my $newFile =  $file;
+    $newFile    =~ s/^$dir//;      # removes base directory from beginning of file name
+    $newFile    =~ s/$startExt$/$endExt/;   # Appends new file with .<convert_ext> instead of .<source_ext>
+    $newFile    =  $dest.$newFile;
+       
+    my $newDir = dirname($newFile);
+    
+    if (! -f $newFile || $force)
     {
-      die "Unable to create $newDir\n";
+    
+      unless(-e $newDir or make_path($newDir))
+      {
+        die "Unable to create $newDir\n";
+      }
+      
+      if ($verbose) { print "Converting $file ... "; }
+        if ($endExt =~ "eps")
+        {
+          system("$cmdStart $file $cmdEnd $epsCmd:$newFile");
+        }
+        else
+        {
+          system("$cmdStart $file $cmdEnd $newFile");
+        }
+          
+      if ($verbose) { print "Done \n"; }
+      
     }
-    
-    if ($verbose) { print "Converting $file ... "; }
-      if ($endExt =~ "eps")
-      {
-        system("$cmdStart $file $cmdEnd $epsCmd:$newFile");
-      }
-      else
-      {
-        system("$cmdStart $file $cmdEnd $newFile");
-      }
-        
-    if ($verbose) { print "Done \n"; }
-    
+    else
+    {
+      if ($verbose) { print "Skipping $file\n"; }
+    }
+  
   }
-  else
-  {
-    if ($verbose) { print "Skipping $file\n"; }
-  }
-
+  print "Conversion complete!\n\n";
 }
-
-print "Conversion complete!\n\n";
+else
+{
+  find({wanted => \&find_file, no_chdir=>1}, $dir);
+  foreach my $file (@filesToConvert)
+  {
+    if ($verbose) {print "Found $file ... ";}
+    system("perl -pi.bak -e 's/\.eps/\.png/g;' $file");
+    if ($verbose) {print "Done!\n";}
+    if ($clean) {unlink $file.'.bak';}
+  }
+  print "Find/Replace complete!\n\n";
+}
 
 # Routine to find file
 sub find_file {
