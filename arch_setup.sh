ce6ddef2c505688493ec41f8071da2a25290bf0b
Kevin Fronczak <kfronczak@gmail.com>
2021-02-08 08:32:10 -0500

    Separated into install and config steps

--- arch_setup.sh
+++ arch_setup.sh
@@ -6,135 +6,139 @@
 # Maybe I'll neevr use it again, but at least I have the steps recorded...
 #=======================================================================================
 
-#------------------------
-# Installation
-#------------------------
-timedatectl set-ntp true
-echo "Creating partition table"
-fdisk -l
-echo "Enter main drive"
-read var_partition
-(
-printf "g\n" # Create guid partition table
-printf "n\n" # Create new parition
-printf "1\n" # Parition 1
-printf "\n" # Default block size
-printf "+512M\n" # Partition size
-printf "t\n" # Change partition file system
-printf "1\n" # Change to EFI system
-printf "n\n" # Add new partition
-printf "2\n" # Partition 2
-printf "\n" # Default size
-printf "\n" # Default sizefd
-printf "w\n" # Write changes to disk
-) | fdisk $var_partition
+if [ $1 = "config" ]; then
+  echo "Enter main drive:
+  read var_partition
 
-fdisk -l | grep $var_partition
+  #------------------------
+  # Configuration
+  #------------------------
+  echo "Updating system clock"
+  echo "Enter timezone (ie. America/New_York)"
+  read var_timezone
+  timedatectl set-timezone $var_timezone
+  hwclock --systohc
+  timedatectl status 
+  read -p "If correct, press any key to continue..."
 
-echo "Creating UEFI and root filesystem"
-mkfs.fat -F32 ${var_partition}1
-mkfs.ext4 ${var_partition}2
-fsck -N ${var_partition}1
-fsck -N ${var_partition}2
+  echo "Setting Locale"
+  vim +%s/#en_US\.UTF-8/en_US\.UTF-8/g +wq /etc/locale.gen
+  locale-gen
+  echo LANG=en_US.UTF-8 > /etc/locale.conf
+  export LANG=en_US.UTF-8
 
-echo "Mount partitions"
-mount ${var_partition}2 /mnt
-mkdir -p /mnt/boot
+  echo "Set hostname"
+  read var_hostname
+  hostnamectl set-hostname $var_hostname
+  echo $var_hostname > /etc/hostname
+  ip addr
+  echo "Enter static IP"
+  read var_staticip
+  touch /etc/hosts
+  echo "127.0.0.1          localhost" > /etc/hosts
+  echo "::1                localhost" >> /etc/hosts
+  echo "${var_staticip}       ${var_hostname}" >> /etc/hosts
+  less /etc/hosts
 
-echo "Sync pacman repo and use fast mirrors"
-pacman -Syy --noconfirm
-pacman -S --noconfirm reflector
-reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+  echo "Set root password"
+  passwd
 
-echo "Installing with pacstrap"
-pacstrap /mnt base linux linux-firmware dhcpcd vim nano
+  #------------------------
+  # Bootloader - GRUB
+  #------------------------
+  echo "Install Grub bootloader"
+  mount -t auto ${var_partition}1 /boot
+  pacman -Syy
+  pacman -S --noconfirm grub efibootmgr
+  grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
+  grub-mkconfig -o /boot/grub/grub.cfg
 
-echo "Configuring system"
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+  #------------------------
+  # Desktiop - XFCE
+  #------------------------
+  echo "Install desktop environment"
+  pacman -S --noconfirm xorg
+  pacman -S --noconfirm xfce4 xfce4-goodies
+  pacman -S --noconfirm lxdm
+  systemctl enable lxdm.service
+  vim +%s/# session/session/g /etc/lxdm/lxdm.conf
 
-#------------------------
-# Configuration
-#------------------------
-echo "Updating system clock"
-echo "Enter timezone (ie. America/New_York)"
-read var_timezone
-timedatectl set-timezone $var_timezone
-hwclock --systohc
-timedatectl status 
-read -p "If correct, press any key to continue..."
+  #------------------------
+  # Packages
+  #------------------------
+  echo "Install essential tools"
+  pacman -S --noconfirm base
+  pacman -S --noconfirm base-devel
+  pacman -S --noconfirm git curl tmux python python-pip
+  pacman -S p7zip unrar tar rsync
+  pacman -S chromium
 
-echo "Setting Locale"
-236 
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-export LANG=en_US.UTF-8
+  echo "Install ssh"
+  pacman -S --noconfirm openssh
+  systemctl enable sshd
 
-echo "Set hostname"
-read var_hostname
-hostnamectl set-hostname $var_hostname
-echo $var_hostname > /etc/hostname
-ip addr
-echo "Enter static IP"
-read var_staticip
-touch /etc/hosts
-echo "127.0.0.1          localhost" > /etc/hosts
-echo "::1                localhost" >> /etc/hosts
-echo "${var_staticip}       ${var_hostname}" >> /etc/hosts
-less /etc/hosts
+  #------------------------
+  # User creation
+  #------------------------
+  visudo /etc/sudoers
+  echo "Create user"
+  echo "Enter user name"
+  read var_user
+  useradd -m -G wheel $var_user
+  passwd $var_user
 
-echo "Set root password"
-passwd
+  read -p "Press any key to reboot..."
 
-#------------------------
-# Bootloader - GRUB
-#------------------------
-echo "Install Grub bootloader"
-mount -t auto ${var_partition}1 /boot
-pacman -Syy
-pacman -S --noconfirm grub efibootmgr
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot
-grub-mkconfig -o /boot/grub/grub.cfg
+  exit
+  reboot
+else
+  #------------------------
+  # Installation
+  #------------------------
+  timedatectl set-ntp true
+  echo "Creating partition table"
+  fdisk -l
+  echo "Enter main drive"
+  read var_partition
+  (
+  printf "g\n" # Create guid partition table
+  printf "n\n" # Create new parition
+  printf "1\n" # Parition 1
+  printf "\n" # Default block size
+  printf "+512M\n" # Partition size
+  printf "t\n" # Change partition file system
+  printf "1\n" # Change to EFI system
+  printf "n\n" # Add new partition
+  printf "2\n" # Partition 2
+  printf "\n" # Default size
+  printf "\n" # Default sizefd
+  printf "w\n" # Write changes to disk
+  ) | fdisk $var_partition
 
+  fdisk -l | grep $var_partition
 
-#------------------------
-# Desktiop - XFCE
-#------------------------
-echo "Install desktop environment"
-pacman -S --noconfirm xorg
-pacman -S --noconfirm xfce4 xfce4-goodies
-pacman -S --noconfirm lxdm
-systemctl enable lxdm.service
-vim +%s/# session/session/g /etc/lxdm/lxdm.conf
+  echo "Creating UEFI and root filesystem"
+  mkfs.fat -F32 ${var_partition}1
+  mkfs.ext4 ${var_partition}2
+  fsck -N ${var_partition}1
+  fsck -N ${var_partition}2
 
-#------------------------
-# Packages
-#------------------------
-echo "Install essential tools"
-pacman -S --noconfirm base
-pacman -S --noconfirm base-devel
-pacman -S --noconfirm git curl tmux python python-pip
-pacman -S p7zip unrar tar rsync
-pacman -S chromium
+  echo "Mount partitions"
+  mount ${var_partition}2 /mnt
+  mkdir -p /mnt/boot
 
-echo "Install ssh"
-pacman -S --noconfirm openssh
-systemctl enable sshd
+  echo "Sync pacman repo and use fast mirrors"
+  pacman -Syy --noconfirm
+  pacman -S --noconfirm reflector
+  reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
 
-#------------------------
-# User creation
-#------------------------
-visudo /etc/sudoers
-echo "Create user"
-echo "Enter user name"
-read var_user
-useradd -m -G wheel $var_user
-passwd $var_user
+  echo "Installing with pacstrap"
+  pacstrap /mnt base linux linux-firmware dhcpcd vim nano
 
-read -p "Press any key to reboot..."
-
-exit
-reboot
+  echo "Configuring system"
+  genfstab -U /mnt >> /mnt/etc/fstab
+  arch-chroot /mnt
+fi
 
 #------------------------
 # Post install
